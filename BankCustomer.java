146b741962d0d358a343a9f8e2b4df86e48ab430
Kushal0504 <kushalgaikwad04@gmail.com>
2025-05-14 03:02:15 -0700

    Save

---
+++ BankCustomer.java
@@ -0,0 +1,17 @@
+class BankCustomer {
+    String name;
+    String accountNumber;
+    int age;
+
+    BankCustomer(String name, String accountNumber, int age) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.age = age;
+    }
+
+    void displayDetails() {
+        System.out.println("Customer Name: " + name);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Customer Age: " + age);
+    }
+}
